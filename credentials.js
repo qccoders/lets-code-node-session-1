dfe3567b9b173b871971c9e8d4d46406da6f9417
Michael Liendo <model3volution@gmail.com>
2017-03-20 08:34:11 -0500

    updated comments

--- credentials.js
+++ credentials.js
@@ -1,3 +1,6 @@
+//How to export your own functions, variables and objects in Node
+//Note that these app credentials have been deprecated and no longer work.
+//Please create your own at instagram.com/developer
 module.exports = {
   CLIENT_ID:'82d957a8aace4d6ea06105d11838a79a',
   CLIENT_SECRET:'12431416c2fa446182e0b630e049bf8d',
